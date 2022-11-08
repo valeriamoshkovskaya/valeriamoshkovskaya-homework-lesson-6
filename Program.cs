7dcae85aa6e30c75b33dc07a53d0c52f2f4ec2fd
valeriamoshkovskaya <leramoshkovskaya@gmail.com>
2022-11-08 15:52:50 +0300

    completed C# BranchesAndLoops

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿ExploreIf();
+void ExploreIf()
+{
+int a = 5;
+int b = 3;
+if (a + b > 10)
+{
+    Console.WriteLine("The answer is greater than 10");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10");
+}
+
+int c = 4;
+if ((a + b + c > 10) && (a == b))
+{
+    Console.WriteLine("The answer is greater than 10");
+    Console.WriteLine("And the first number is equal to the second");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10");
+    Console.WriteLine("Or the first number is not equal to the second");
+}
+
+if ((a + b + c > 10) || (a == b))
+{
+    Console.WriteLine("The answer is greater than 10");
+    Console.WriteLine("Or the first number is equal to the second");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10");
+    Console.WriteLine("And the first number is not equal to the second");
+}
+}
+
+ExploreIf();
+
+int counter = 0;
+do
+{
+    Console.WriteLine($"Hello World! The counter is {counter}");
+    counter++;
+} while (counter < 10);
+
+do
+{
+    Console.WriteLine($"Hello World! The counter is {counter}");
+    counter++;
+} while (counter < 10);
+
+for (int index = 2; index < 10; index++)
+{
+    Console.WriteLine($"Hello World! The index is {index}");
+}
+
+ExploreLoops();
+void ExploreLoops()
+{
+for (int row = 1; row < 11; row++)
+{
+    for (char column = 'a'; column < 'k'; column++)
+    {
+        Console.WriteLine($"The cell is ({row}, {column})");
+    }
+}
+}
+
+
+    int sum = 0;
+    for (int number = 1; number < 21; number++)
+    {
+        if (number % 3 == 0)
+        {
+            sum = sum + number;
+        }
+    }
+    Console.WriteLine($"The sum is {sum}");
